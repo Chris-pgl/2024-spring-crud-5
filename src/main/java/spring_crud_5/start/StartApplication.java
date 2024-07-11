89119555614d2affd63b5991e9377e64b9aa3134
Chris <cr1s@hotmail.it>
2024-07-11 11:51:03 +0200

    Add new project

---
+++ src/main/java/spring_crud_5/start/StartApplication.java
@@ -0,0 +1,59 @@
+package spring_crud_5.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+
+}
+
+/**
+	 * REPO: 2024-spring-crud-5
+	 * 
+	 * TODO:
+	 * 
+	 * In nuovo esercizio sviluppare un'applicazione per gestire le seguenti
+	 * entita':
+	 * 
+	 * Customer (cliente):
+	 * - id : INT
+	 * - name : STRING
+	 * - lastname : STRING
+	 * - email : STRING
+	 * - phone : STRING
+	 * 
+	 * Order (ordine):
+	 * - id : INT
+	 * 
+	 * Product (prodotto):
+	 * - id : INT
+	 * - name : STRING
+	 * - price : INT
+	 * - vat (iva percentuale) : INT
+	 * 
+	 * Un cliente puo' effettuare piu' ordini, ogni ordine contiene piu' prodotti.
+	 * 
+	 * La classe Order deve inoltre esporre il seguente metodo:
+	 * - getFullPrice(boolean vat) : INT --> restituisce il prezzo totale
+	 * dell'ordine
+	 * comprensivo o meno dell'iva in base al
+	 * parametro booleano in ingresso
+	 * 
+	 * La classe Product deve inoltre esporre il seguente metodo:
+	 * - getFullPrice(boolean vat) : INT --> restituisce il prezzo totale del
+	 * prodotto
+	 * maggiorato dell'iva se la variabile booleana
+	 * in ingresso e' true
+	 * 
+	 * Definire sensatamente le relazioni e testare adeguatamente sia classi che
+	 * relazioni man mano che vengono implementate.
+	 * 
+	 * E' possibile definire altre classi/interfacce qualora lo si ritenga
+	 * oppurtuno.
+	 * 
+	 */
